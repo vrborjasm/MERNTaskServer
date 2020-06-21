3d05a817cd0970d4ce3b84a71c8f472f67551716
vrborjasm <32269568+vrborjasm@users.noreply.github.com>
2020-06-21 10:17:40 -0400

    adjust for deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use( express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
@@ -18,6 +18,6 @@ app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
 
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
